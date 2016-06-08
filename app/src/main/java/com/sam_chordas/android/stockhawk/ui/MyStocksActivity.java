5caa0dd5cdabad0282a0ac6bde485459c17e60cc
neerajms <neerajms99b@gmail.com>
2016-06-08 17:30:33 +0530

    Minor changes to appbar icons

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -81,7 +81,7 @@ public class MyStocksActivity extends AppCompatActivity implements LoaderManager
             mProgress.isIndeterminate();
             mProgress.show();
         }
-        
+
         mSavedInstanceState = savedInstanceState;
         mContext = this;
         ConnectivityManager cm =
@@ -305,9 +305,9 @@ public class MyStocksActivity extends AppCompatActivity implements LoaderManager
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         if (id == R.id.action_change_units) {
             // this is for changing stock changes from percent value to dollar value
